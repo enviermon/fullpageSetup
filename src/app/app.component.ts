6954718e4d641da61ce188dea8cdce518bd24f7d
enviermon <falk.hofmann89@gmx.de>
2017-04-01 12:14:56 +0200

    service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,8 @@
 import {AfterViewInit, Component, OnChanges, OnInit, ViewChild} from '@angular/core';
-import {User} from "./user";
 import {MnFullpageDirective} from "./fullpagetest/mnFullpage.directive";
+import {Hero} from "./service/hero";
+import {HeroSerivce} from "./service/hero.service";
+import {User} from "./service/user";
 
 @Component({
   selector: 'my-app',
@@ -10,22 +12,34 @@ import {MnFullpageDirective} from "./fullpagetest/mnFullpage.directive";
 export class AppComponent implements OnInit, AfterViewInit, OnChanges{
   @ViewChild(MnFullpageDirective) vc:MnFullpageDirective;
 
+  heroes: Hero[];
+  private anyErrors: any;
+  users = [
+    new User('Mahesh', 0),
+    new User('Mahesh', 2)
+  ];
+  constructor(private heroService: HeroSerivce){}
+
   ngOnChanges(): void {
     console.log("appcomponent ngOnChanges");
   }
 
-  users = [
-    new User('Mahesh', 0),
-    new User('Test', 2)
-
-  ];
-
   ngAfterViewInit(): void {
     console.log("appcomponent ngAfterViewInit");
     this.vc.callme();
-  }
-  ngOnInit(): void {
 
+  }
+
+  ngOnInit(): void {
+    this.getHeroes();
     console.log("appcomponent ngOnInit");
+  }
+
+  getHeroes():void {
+    this.heroService.findAll().subscribe(
+        hero => this.heroes = hero,
+        error => this.anyErrors = error
+    );
+
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import {MnFullpageDirective} from "./fullpagetest/mnFullpage.directive";
+import {HeroSerivce} from "./service/hero.service";
 
 @NgModule({
   imports: [BrowserModule],
   declarations: [AppComponent, MnFullpageDirective],
+  providers: [HeroSerivce],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
